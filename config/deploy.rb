c00e23cbc8ccdaca071179eb5ab3ddc8f0f1ac80
Yuki Nakamuraya <fourdim53@gmail.com>
2018-04-13 11:51:47 +0900

    change private key stored directory specification to tilde

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/yamato48/.ssh/ab_key_pair.pem']
+                  keys: ['~/.ssh/ab_key_pair.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
