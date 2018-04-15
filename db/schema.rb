68b15bc971d1d2865d108683a5405f5ac0dfbb3b
yamato48 <yamd1048@gmail.com>
2018-04-15 14:44:36 +0900

    Enum convert japanese
    ユーザーモデルもenum日本語化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410034824) do
+ActiveRecord::Schema.define(version: 20180413103845) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 20180410034824) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name",                           null: false
+    t.integer  "sex"
+    t.date     "birth_year"
+    t.date     "birth_month"
+    t.date     "birth_day"
+    t.string   "phone_number"
+    t.integer  "language"
+    t.integer  "currency"
+    t.string   "identification"
+    t.string   "address"
+    t.string   "profile"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
