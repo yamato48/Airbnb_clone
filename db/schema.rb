3739a70ee514722ba28001698cadb3255d90659d
Maeda Kanako <knk.itg.milk@gmail.com>
2018-04-16 13:04:02 +0900

    add-column
    usersの任意項目のカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 20180415033132) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.date     "birth_year"
+    t.date     "birth_month"
     t.string   "first_name",                          null: false
     t.string   "last_name",                           null: false
     t.date     "birth_day",                           null: false
@@ -71,6 +73,8 @@ ActiveRecord::Schema.define(version: 20180415033132) do
     t.string   "identification"
     t.string   "address"
     t.string   "profile"
+    t.string   "school"
+    t.string   "workplace"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
