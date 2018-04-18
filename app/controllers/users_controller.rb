ea549689f8279b9990329784b53029090f35e321
Maeda Kanako <knk.itg.milk@gmail.com>
2018-04-18 12:40:35 +0900

    add-contoroller-action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
-  def index
-  end
 
   def edit
+  	@user = current_user
   end
 
-  def show
+  def update
+  	if current_user.update(user_params)
+  		flash.now[:alert]= 'プロフィールが正常に更新されました'
+  		redirect_to root_path
+  	else
+  		render :edit
+  	end
+  end
+
+  def edit_varification
+  end
+
+  def media
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:email, :last_name, :first_name, :sex, :birth_day, :phone_number, :language, :currency, :address, :profile, :school, :workplace )
   end
 end
